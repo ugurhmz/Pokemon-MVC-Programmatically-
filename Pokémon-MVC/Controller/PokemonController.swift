1b8a80c4655a3776cc47255a6f26aae09c61bc18
ugurhmz <craxx67@gmail.com>
2022-02-21 02:57:42 +0300

    PoemonCell created then registered with reuseId

--- Pokémon-MVC/Controller/PokemonController.swift
+++ Pokémon-MVC/Controller/PokemonController.swift
@@ -1,36 +1,28 @@
 
 import UIKit
 
+private let reuseId = "PokemonCell"
 
 
 class PokemonController: UICollectionViewController {
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-       
-        
         configureViewComponents()
     }
-    
-    
     
     // searching
    @objc func searchFunc(){
         print(15131231)
     }
     
-    
-    
-    
     // MARK - Helper Func
     func configureViewComponents(){
-   
+        
         
         if #available(iOS 13.0, *) {
                 let navBarAppearance = UINavigationBarAppearance()
-                navBarAppearance.configureWithOpaqueBackground()
+                //navBarAppearance.configureWithOpaqueBackground()
             
                 navBarAppearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white,
                                                     NSAttributedString.Key.font: UIFont(name: "Chalkduster", size: 28.0)!  ]
@@ -44,13 +36,33 @@ class PokemonController: UICollectionViewController {
                 navigationItem.title = "Pok∑mon®"
 
             }
-            
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(searchFunc))
         navigationItem.rightBarButtonItem?.tintColor = .white
         
+        
+        // cell register
+        collectionView.register(PokemonCell.self, forCellWithReuseIdentifier: reuseId)
     }
-    
     
 }
 
+
+
+
+
+// collectionView
+extension PokemonController {
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseId, for:indexPath) as! PokemonCell
+        
+        return cell
+    }
+    
+}
---
+++ Pokémon-MVC/View/PokemonCell.swift
@@ -0,0 +1,22 @@
+
+
+import UIKit
+
+
+class PokemonCell : UICollectionViewCell {
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init coder implement err!!")
+    }
+    
+    
+}
+
+
+
