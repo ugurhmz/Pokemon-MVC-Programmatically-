b5e899a9cb22e31bb8e4339ec132bb9170078e05
ugurhmz <craxx67@gmail.com>
2022-02-21 04:15:30 +0300

    anchor created NSLayoutYAxisAnchor, XaisAnchor

--- Pokémon-MVC/Utils/Extensions.swift
+++ Pokémon-MVC/Utils/Extensions.swift
@@ -4,7 +4,7 @@ import UIKit
 
 
 
-
+// UIColor
 extension UIColor {
     
     
@@ -25,13 +25,58 @@ extension UIColor {
 
 
 
-
+// UIView
 extension UIView {
     
     func center(inView view:UIView) {
         self.translatesAutoresizingMaskIntoConstraints = false
         self.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         self.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    
+    func anchor(top: NSLayoutYAxisAnchor?,
+                left: NSLayoutXAxisAnchor?,
+                bottom: NSLayoutYAxisAnchor?,
+                right: NSLayoutXAxisAnchor?,
+                paddingTop : CGFloat,
+                paddingLeft : CGFloat,
+                paddingBottom : CGFloat,
+                paddingRight : CGFloat,
+                width: CGFloat,
+                height: CGFloat )
+    {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        
+        
+        if let left = left {
+            self.leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        
+        
+        if let bottom = bottom {
+            self.bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+        
+        if let right = right {
+            self.rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        
+        
+        if width != 0 {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if height  != 0 {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+        
     }
     
 }
